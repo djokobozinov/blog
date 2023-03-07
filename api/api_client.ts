91328fb0735231134162fe309d28533bdadd0fa1
Gjoko Bozhinov <djoko.bozinov@gmail.com>
2023-03-07 12:54:02 +0100

    Small bug fix.

--- api/api_client.ts
+++ api/api_client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const ApiClient = axios.create({
-	baseURL: `http://localhost:3000/api/`,
+	baseURL: `https://gbozhinov.com/api/`,
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../services/post_service';
