4bfa49e74b3cf6043eb28f6b77bd620774f3652f
Gjoko <djoko.bozinov@gmail.com>
2023-12-18 10:15:48 +0100

    Small design change.

--- components/youtube_video.tsx
+++ components/youtube_video.tsx
@@ -1,12 +1,11 @@
 export default function YouTubeVideo({ videoId }) {
 	return (
-		<div className='flex w-full justify-center border rounded-lg py-5'>
+		<div className='flex w-full justify-center'>
 			<iframe
-				width='560'
-				height='315'
+				width='612'
+				height='400'
 				src={`https://www.youtube.com/embed/${videoId}`}
 				title='Video introduction'
-				frameBorder='0'
 				allowFullScreen
 			></iframe>
 		</div>
