0ebd26f1836cfe1128dc96b2c743e8ac62622adc
Gjoko <djoko.bozinov@gmail.com>
2024-01-16 11:14:30 +0100

    My tech wip.

---
+++ components/lib_item.tsx
@@ -0,0 +1,10 @@
+export default function LibItem({ title, description, url }) {
+	return (
+		<div className='flex'>
+			<a className={'text-cyan-900 font-semibold w-[80px] shrink-0'} href={url} target='_blank'>
+				{title}
+			</a>
+			<p>{description}</p>
+		</div>
+	);
+}
---
+++ pages/mytech.tsx
@@ -0,0 +1,26 @@
+import Layout from '../components/layout';
+import LibItem from '../components/lib_item';
+
+export default function Page() {
+	return (
+		<Layout title={'My tech'} description={'My tech choice & favorite libraries'} home>
+			<div className='flex flex-col mt-7 gap-3'>
+				<h1 className='text-2xl text-cyan-900 mb-5'>My tech choice & favorite libraries</h1>
+				<LibItem
+					title={'Next.js'}
+					description={
+						'Next.js is my favorite React framework. It is a React framework that provides a lot of features out of the box, such as server-side rendering, static site generation, and more.'
+					}
+					url={'https://nextjs.org/'}
+				/>
+				<LibItem
+					title={'Lucid'}
+					description={
+						'Lucide is an open-source icon library that provides 1000+ vector (svg) files for displaying icons and symbols in digital and non-digital projects.'
+					}
+					url={'https://lucide.dev/'}
+				/>
+			</div>
+		</Layout>
+	);
+}
