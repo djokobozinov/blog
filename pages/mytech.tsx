401750549dede5035a29fbafbf99e263977f1d34
Gjoko <djoko.bozinov@gmail.com>
2024-01-16 11:59:30 +0100

    My tech update.

--- pages/mytech.tsx
+++ pages/mytech.tsx
@@ -3,8 +3,8 @@ import LibItem from '../components/lib_item';
 
 export default function Page() {
 	return (
-		<Layout title={'My tech'} description={'My tech choice & favorite libraries'} home>
-			<div className='flex flex-col mt-7 gap-3'>
+		<Layout title={'My tech'} description={'My tech choice & favorite libraries'} home={false}>
+			<div className='flex flex-col gap-3'>
 				<h1 className='text-2xl text-cyan-900 mb-5'>My tech choice & favorite libraries</h1>
 				<LibItem
 					title={'Next.js'}
@@ -12,6 +12,11 @@ export default function Page() {
 						'Next.js is my favorite React framework. It is a React framework that provides a lot of features out of the box, such as server-side rendering, static site generation, and more.'
 					}
 					url={'https://nextjs.org/'}
+				/>
+				<LibItem
+					title={'Tailwind CSS'}
+					description={'Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.'}
+					url={'https://tailwindcss.com/'}
 				/>
 				<LibItem
 					title={'Lucid'}
