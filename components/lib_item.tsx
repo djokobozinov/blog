2fcd4729a0143fff35bde8aade64f6ca909bc5b3
Gjoko <djoko.bozinov@gmail.com>
2024-01-16 12:07:14 +0100

    My tech item design improvement.

--- components/lib_item.tsx
+++ components/lib_item.tsx
@@ -1,10 +1,10 @@
 export default function LibItem({ title, description, url }) {
 	return (
-		<div className='flex'>
+		<div className='flex flex-col md:flex-row'>
 			<a className={'text-cyan-900 font-semibold w-[140px] shrink-0'} href={url} target='_blank'>
 				{title}
 			</a>
-			<p>{description}</p>
+			<p className='text-sm'>{description}</p>
 		</div>
 	);
 }
